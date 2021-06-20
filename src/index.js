a97472d467d24d1296320be185a6fc6558314b17
khanh091020 <86177733+khanh091020@users.noreply.github.com>
2021-06-20 13:40:25 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -57,6 +57,6 @@ app.post('/news',(req,res) => {
 // function route
 routes(app);
 // port listener 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
