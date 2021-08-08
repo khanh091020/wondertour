ca6672d182615be336bc9944f738e25b45f1243b
khanh091020 <86177733+khanh091020@users.noreply.github.com>
2021-08-08 19:34:34 +0700

    x

--- src/routes/adminRoute/createTour.js
+++ src/routes/adminRoute/createTour.js
@@ -5,7 +5,7 @@ const multer = require('multer')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './src/public/image')
+      cb(null, '/image')
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + '-' + file.originalname)
