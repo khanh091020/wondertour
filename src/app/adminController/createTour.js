df86ab9a00ba15759099943a6f7daafa4ccf36c6
khanh091020 <86177733+khanh091020@users.noreply.github.com>
2021-08-08 19:18:25 +0700

    ccc

--- src/app/adminController/createTour.js
+++ src/app/adminController/createTour.js
@@ -11,10 +11,15 @@ class creatTour{
         req.body.img1 = 'image/'+ req.body.img1 ;
         req.body.img2 = 'image/'+ req.body.img2 ;
         req.body.img3 = 'image/'+ req.body.img3 ;
+        req.body.img4 = 'image/'+ req.body.img4 ;
         const object = new tour(req.body);
         object.save()
-      
         res.send(object);
+    }
+
+    // post uplouad img tour 
+    uploadImg(req, res, next) {
+       res.send(req.files)
     }
 } 
 
--- src/routes/adminRoute/createTour.js
+++ src/routes/adminRoute/createTour.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const route = express.Router();
 const controller = require('../../app/adminController/createTour');
+const multer = require('multer')
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './src/public/image')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname)
+    }
+  })
+
+  function fileFilters (req, file, cb) {
+    
+    if(!file.originalname.match(/\.(jpg|png|gif|jpeg|svg|webp)$/))
+    {
+        cb(new Error('I don\'t have a clue!'))
+        cb(null, false)
+    }
+    else
+    cb(null, true)
+  }
+
+  var upload = multer({ storage: storage, fileFilter: fileFilters})
+
+route.post('/upload__img-tour',upload.any(),controller.uploadImg)
 route.get('/',controller.show);
 route.post('/',controller.save)
 module.exports = route;
