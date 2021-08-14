db62dbdbd1c32e27905b3946f52db6d07e0c0708
khanh091020 <86177733+khanh091020@users.noreply.github.com>
2021-08-14 18:24:27 +0700

    ok

--- src/app/adminController/createTour.js
+++ src/app/adminController/createTour.js
@@ -7,19 +7,11 @@ class creatTour{
     }
 
     async save(req,res,next) {
-        req.body.img = 'image/'+ req.body.img ;
-        req.body.img1 = 'image/'+ req.body.img1 ;
-        req.body.img2 = 'image/'+ req.body.img2 ;
-        req.body.img3 = 'image/'+ req.body.img3 ;
-        req.body.img4 = 'image/'+ req.body.img4 ;
+       
         tour.create(req.body)
         res.redirect('/admin/createTour')
     }
 
-    // post uplouad img tour 
-    uploadImg(req, res, next) {
-       res.send(req.files)
-    }
 } 
 
 module.exports = new creatTour;
--- src/routes/adminRoute/createTour.js
+++ src/routes/adminRoute/createTour.js
@@ -2,6 +2,15 @@ const express = require('express');
 const route = express.Router();
 const controller = require('../../app/adminController/createTour');
 const multer = require('multer')
+const fs = require('fs')
+var cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+  cloud_name: 'wonder-place', 
+  api_key: '294997753685562', 
+  api_secret: '2R5HClFhxlUNLBg5lp9FtV3WHlA',
+  secure: true
+});
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,9 +32,56 @@ var storage = multer.diskStorage({
     cb(null, true)
   }
 
-  var upload = multer({ storage: storage, fileFilter: fileFilters})
+  // Delete image only admin can use
+route.post('/destroy', (req, res) =>{
+  try {
+      const {public_id} = req.body;
+      if(!public_id) return res.status(400).json({msg: 'No images Selected'})
 
-route.post('/upload__img-tour',upload.any(),controller.uploadImg)
+      cloudinary.v2.uploader.destroy(public_id, async(err, result) =>{
+          if(err) throw err;
+
+          res.json({msg: "Deleted Image"})
+      })
+
+  } catch (err) {
+      return res.status(500).json({msg: err.message})
+  }
+  
+})
+
+  
+const removeTmp = (path) =>{
+  fs.unlink(path, err=>{
+      if(err) throw err;
+  })
+}
+
+  var upload = multer({ storage: storage,limits : {
+    files : 5,
+  }, fileFilter: fileFilters})
+
+  function uploadToCloudinary(path) {
+    return new Promise((resolve, reject) => {
+      cloudinary.v2.uploader.upload(path,{folder: "image"}, (err, data) => {
+        if (err) return reject(err);
+        removeTmp(path)
+        return resolve(data.secure_url);
+      })
+    });
+  }
+
+route.post('/upload__img-tour',upload.any(),async function(req, res,next) {
+   var urls = new Array();
+  for (let file of req.files) {
+   const data = await uploadToCloudinary(file.path);
+   urls.push(data);
+  }
+  res.send(urls);
+})
 route.get('/',controller.show);
 route.post('/',controller.save)
 module.exports = route;
+
+
+
