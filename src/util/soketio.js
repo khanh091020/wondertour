0d46fda1ae43cef1bd1f100c233413aa5d45a72f
khanh091020 <86177733+khanh091020@users.noreply.github.com>
2021-08-20 09:51:22 +0700

    Update soketio.js

--- src/util/soketio.js
+++ src/util/soketio.js
@@ -29,7 +29,7 @@ module.exports = function (io) {
             // let data = {
             //     sender : "WONDER PLACE",
             //     message : "Welcome you to wonder place support !",
-            //     time : moment.utc(new Date().getUTCDate).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss')
+            //     time : moment.utc(Date.now()).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss')
             // }
             // socket.emit('update_message', data);
              
@@ -68,14 +68,14 @@ module.exports = function (io) {
             let data = {
                 sender : "You",
                 message : message,
-                time : moment.utc(new Date().getUTCDate).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss')
+                time : moment.utc(Date.now()).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss')
             }
             socket.emit("update_message",data);
             
              data = {
                 sender : socket.name,
                 message : message,
-                time : moment.utc(new Date().getUTCDate).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss'),
+                time : moment.utc(Date.now()).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss'),
                
             }
             socket.to(socket.room).emit("update_message",data);
@@ -125,7 +125,7 @@ module.exports = function (io) {
         //   let data = {
         //       sender : "WONDER PLACE",
         //       message : "Support is starting online !",
-        //       time : moment.utc(new Date().getUTCDate).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss')
+        //       time : moment.utc(Date.now()).tz("Asia/Saigon").format('DD/MM/YYYY HH:mm:ss')
         //   }
         //   socket.broadcast.emit('update_message', data)
         
