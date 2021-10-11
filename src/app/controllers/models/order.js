a7d010d63bf7068b5d8f3e9250bf790fe89b48d9
Nguyen Ngoc Khanh <nnkhanh.20it12@vku.udn.vn>
2021-10-11 13:07:23 +0700

    Update order.js

--- src/app/controllers/models/order.js
+++ src/app/controllers/models/order.js
@@ -1,21 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const order = new Schema({
     userName: {type : String},
     phone : {type : Number},
-    userEmail : {type : String, ref:'account'},
-    tourDate : {type : String},
-    tourID : {type : Number,ref : 'Tours'},
-    photographer : {type : String},
-    singerTour : {type : Boolean},
-    adultNumber : {type :Number},
-    chilrenNumber : {type : Number},
-    totalPrice : {type : Number},
-    status : {type : String},
-    paid :  {type : Boolean}
-},{
-    timestamps : true
-})
-
-module.exports = mongoose.model('orders',order);
