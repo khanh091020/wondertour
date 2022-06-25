c4d029920a11d77cd123caf2c91d9e0ec0bd9989
khanh091020 <86177733+khanh091020@users.noreply.github.com>
2022-06-25 09:58:41 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
-      "mongodb+srv://khanh:hanCqaJG0CLJlE1O@cluster0.gkzod.mongodb.net/travelweb?retryWrites=true&w=majority",
+      "mongodb+srv://new__User1:MuZzhzT4HWDNYEzK@cluster0.gkzod.mongodb.net/travelweb?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
